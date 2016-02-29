229134b0b2ba1fc2e6757316d3cfd376151710ce
Arshan <akhanifa@uwaterloo.ca>
2016-02-29 17:34:43 -0500

    fixed the path and settimeout

--- base_road07/app.js
+++ base_road07/app.js
@@ -1,6 +1,6 @@
 var s = Snap('#sky');
 
-var defaultStyleColors=['url(#radial-gradient)','#29abe2','#333','#666','#999','#4d4d4d','#b3b3b3','gray','#fff'];
+var defaultStyleColors=['url(#radial-gradient)','#29abe2','#333','#666','#676767','#4d4d4d','#676767','gray','#fff'];
 function loadStuff(){
 	defaultStyleColors.forEach(function(element,index){
 		if(index!=1){
@@ -44,9 +44,10 @@ setTimeout(fadeStroke('.constellation1,.constellation2',0,0),0);
 
 
 // adjusting the Road Data
+hide('#roadDataMask')();
+s.selectAll('#roadDataBackground path').animate({d:"M970,1420 970,1420 970.9,1420 970.9,1420"},0);
+setTimeout(slide('#roadDataMask',508,1239,0),0);
 
-s.selectAll('#data_object').attr({display:'none'});
-setTimeout(slide('#data_object',-150,-600,0),0);
 
 
 // adjusting the buildings Data 
@@ -67,7 +68,7 @@ setTimeout(changeScale('#treeDataNumbers',1,0,969,1070,0),0);
 
 setTimeout(slide('#data_hydroPoles',485,88,0),0);
 s.selectAll('#hydroObject_x5F_right,#hydroObject_x5F_left').attr({display:'none'});
-setTimeout(function(){hydroPolesWiresOut(lengths,0)},0);
+setTimeout(function(){hydroPolesWiresOut(0)},0);
 
 
 
@@ -135,11 +136,12 @@ function fadeStroke(selector,alpha,duration){
 		},duration);
 	}
 }
-function slide(selector,x,y,duration){
+function slide(selector,x,y,duration,easing){
+	if (easing === undefined){easing = mina.linear};
 	return function(){
 		s.selectAll(selector).animate({
 			transform: Translate(x,y)
-		},duration);
+		},duration,easing);
 	}
 }
 function changeViewBox(values,duration){
@@ -154,6 +156,14 @@ function changeFill(selector,color,duration){
 		s.selectAll(selector).animate({
 			fill: color,
 		},duration)
+	}
+}
+function changeStroke(selector,color){
+	return function() {
+		s.selectAll(selector).attr({style:'stroke:'+color});
+		// s.selectAll(selector).animate({
+		// 	style: 'stroke : ' + color,
+		// },duration);
 	}
 }
 
@@ -181,21 +191,105 @@ function setDashoffset(selector,length,duration){
 	}
 }
 
-function doSetTimeout1(func,param1,delay){
-	setTimeout(function(){func(param1)},delay);
+function setTimeoutFixed() {
+	//first param = function
+	var func = arguments[0];
+	var params =[];
+	for (var i = 1 ; i < arguments.length - 1; i++){
+		params.push(arguments[i]);
+	}
+	var delay = arguments[arguments.length - 1];
+	setTimeout(function(){func.apply(null,params)},delay);
 }
-function doSetTimeout2(func,param1,param2,delay){
-	setTimeout(function(){func(param1,param2)},delay);
+
+function draw(selector,duration,reverse,out){
+	return function(){
+		var elements = s.selectAll(selector);
+		elements.forEach(function(element){
+			var length = element.getTotalLength();
+			var offsetInit;
+			var offsetFinal;
+			if(reverse && !out ){
+				offsetInit = -length;
+				offsetFinal = 0;
+			}else if(reverse && out){
+				offsetInit = 0;
+				offsetFinal = length;
+			}else if(!reverse && out){
+				offsetInit = 0;
+				offsetFinal = -length;
+			}else if (!reverse && !out){
+				offsetInit = length;
+				offsetFinal = 0;
+			}
+			element.attr({
+				'stroke-dasharray':length,
+				'stroke-dashoffset':offsetInit,
+				'stroke': '#00FFFF' 
+			});
+			element.animate({'stroke-dashoffset': offsetFinal }, duration);
+		})	
+	}
 }
-function doSetTimeout3(func,param1,param2,param3,delay){
-	setTimeout(function(){func(param1,param2,param3)},delay);
+
+function window1(){
+	for (var i = 1; i < 9 ; i++){
+		setTimeout(changeFill('#_'+i+'-2','#00FFFF',100),i*60);
+	}
+	for (var i = 1; i < 9 ; i++){
+		setTimeout(changeFill('#_'+i+'-2','black',100),480+i*60);
+	}
 }
-function doSetTimeout4(func,param1,param2,param3,param4,delay){
-	setTimeout(function(){func(param1,param2,param3,param4)},delay);
+
+function window2(){
+	var numberRight = s.selectAll('#window2 .right line').length;
+	for (var i = 1; i < numberRight+1 ; i++){
+		setTimeout(changeStroke('#window2 .right line:nth-of-type('+i+')','#00FFFF'),i*1);
+	}
+	for (var i = 1; i < numberRight+1 ; i++){
+		setTimeout(changeStroke('#window2 .right line:nth-of-type('+i+')','#000000'),numberRight*1+i*1);
+	}
+	var numberLeft = s.selectAll('#window2 .left line').length;
+	for (var i = 1; i < numberLeft+1 ; i++){
+		setTimeout(changeStroke('#window2 .left line:nth-of-type('+i+')','#00FFFF'),shift(numberRight*2*1,i*1));
+	}
+	for (var i = 1; i < numberLeft+1 ; i++){
+		setTimeout(changeStroke('#window2 .left line:nth-of-type('+i+')','#000000'),shift(numberRight*2*1,numberLeft*1+i*1));
+	}
 }
-function doSetTimeout5(func,param1,param2,param3,param4,param5,delay){
-	setTimeout(function(){func(param1,param2,param3,param4,param5)},delay);
+
+function window3(){
+	var number = s.selectAll('#window3 line').length;
+	for (var i = 1; i < number+1 ; i++){
+		setTimeout(changeStroke('#window3 line:nth-of-type('+i+')','#00FFFF'),i*10);
+	}
+	for (var i = 1; i < number+1 ; i++){
+		setTimeout(changeStroke('#window3 line:nth-of-type('+i+')','#000000'),number*10+i*10);
+	}
 }
+function window4(){
+	var number = s.selectAll('#window4 line').length;
+	for (var i = 1; i < number+1 ; i++){
+		setTimeout(changeStroke('#window4 line:nth-of-type('+(number+1-i)+')','#00FFFF'),i*10);
+	}
+	for (var i = 1; i < number+1 ; i++){
+		setTimeout(changeStroke('#window4 line:nth-of-type('+(number+1-i)+')','#000000'),number*10+i*10);
+	}
+}
+
+function CNLit(){
+	changeFill('#CN_tower-2','#00FFFF',100)();
+}
+
+function roadLit(){
+	s.selectAll('#roadDataBackground path').animate({d:"M19,1925 940,1167 1000.9,1167 1909,1926"},0,mina.easeout)
+	s.selectAll('#roadDataBackground path').animate({style:'fill:rgba(0,0,0,0)'},0);
+	s.selectAll('#roadDataBackground path').animate({style:'fill:#00FFFF'},1000);
+
+}
+
+
+
 
 function shift(number,tobeAdded){
 	return number+tobeAdded;
@@ -205,32 +299,28 @@ function scale(scalex,scaley,x,y){
 	return "matrix("+scalex+", 0, 0,"+scaley+", "+ (x-scalex*x) +"," + (y-scaley*y) +")";
 }
 
-// var wires_data_left_lengths = [55,90,135,230,634];
-var lengths = [55,90,135,230,634];
-function strokeDanceWireOut(index,length,duration){
-	s.selectAll('#hydroWires .wire' + index + ' path').animate({'stroke-dasharray':length},0);
-	s.selectAll('#hydroWires .wire' + index + ' path').animate({'stroke-dashoffset':-length},duration);
+function strokeDanceWireOut(index,duration){
+	setTimeout(draw('#hydroWires .wire' + index + ' path',duration,false,true),0);
 	s.selectAll('#hydroWires .wire' + index + ' text').attr({'display':'none'});
 }
 
-function strokeDanceWireIn(index,length,duration){
-	s.selectAll('#hydroWires .wire' + index + ' path').animate({'stroke-dasharray':length},0);
-	s.selectAll('#hydroWires .wire' + index + ' path').attr({'stroke-dashoffset':length});
-	s.selectAll('#hydroWires .wire' + index + ' path').animate({'stroke-dashoffset':0},duration);
+function strokeDanceWireIn(index,duration){
+	setTimeout(draw('#hydroWires .wire' + index + ' path',duration,false,false),0);
 	s.selectAll('#hydroWires .wire' + index + ' text').attr({'display':'block'});
 }
 
 
-function hydroPolesWiresOut(lengths,duration){
+function hydroPolesWiresOut(duration){
 	for(var i=1;i<6;i++){
-		doSetTimeout3(strokeDanceWireOut,i,lengths[i-1],duration,(i-1)*duration);
+		setTimeoutFixed(strokeDanceWireOut,i,duration,(i-1)*duration);
 	}
 }
-function hydroPolesWiresIn(lengths,duration){
+function hydroPolesWiresIn(duration){
 	for(var i=1;i<6;i++){
-		doSetTimeout3(strokeDanceWireIn,i,lengths[i-1],duration,(i-1)*duration);
+		setTimeoutFixed(strokeDanceWireIn,i,duration,(i-1)*duration);
 	}
 }
+
 
 
 
@@ -243,8 +333,8 @@ var step1 = 0;
 setTimeout(fadeFill('#road-3',1,500),shift(100,step1));
 setTimeout(slide('#road-3',485.22,74.02,500),shift(100,step1));
 setTimeout(fadeFill('#road rect',1,500),shift(100,step1));
-setTimeout(slide('#road rect',0,0,500),shift(100,step1));		
-
+setTimeout(slide('#road rect',0,0,500),shift(100,step1));	
+setTimeout(show('#roadDataMask'),shift(600,step1));	
 var step2 = 600 ;
 var off2 = 0;
 
@@ -263,20 +353,24 @@ var off3 = 0;
 
 
 
-var step4 = 1800 + off2 + off3;
-// Buildings : 1800-2200
+var step4 = 900 + off2 + off3;
+// Tdott : 900-2200
 //diff : 400
 
 setTimeout(show('#skyline'),shift(0,step4));
 for (var i=1;i<13;i++){
 	if(i<10){
-		setTimeout(slide('#skyline #group0'+i,0,-650,200),shift(100+i*100,step4));
+		setTimeout(slide('#skyline #group0'+i,0,-650,200),shift(i*100,step4));
 	} else{
-		setTimeout(slide('#skyline #group'+i,0,-650,200),shift(100 +i*100 ,step4));
+		setTimeout(slide('#skyline #group'+i,0,-650,200),shift(i*100 ,step4));
 	}
 }
+for (var i = 1 ; i < 6 ; i++){
+	setTimeout(slide('#skyline #window'+i,0,-650,200),shift(500+i*100,step4));
+}
+setTimeout(slide('#skyline #CN_tower-2',0,-650,200),shift(12*100,step4));
 
-var step5 = 2200 + off2 + off3;
+var step5 = 2300 + off2 + off3;
 var off5 = 0;
 
 // Hydro Poles and their wires : 2200-2500
@@ -288,21 +382,25 @@ setTimeout(function(){movingNumbers('#hydroWires text',9,160)},shift(300,step5))
 
 
 
-var off6=3500;
-var step6 = 2500 + off2 + off3 + off5;
-//sky scrapers : 2500-3100
+var off6=5000;
+var step6 = 2000 + off2 + off3 + off5;
+//windows and hydropole wires : 2500-3100
 // diff: 600
 
+setTimeout(window4,shift(500,step6));
+setTimeout(window3,shift(900,step6));	
+setTimeout(window1,shift(1400,step6));
+setTimeout(window2,shift(1000,step6));
+setTimeout(CNLit,shift(2000,step6));
 
-setTimeout(fadeFill('#skyscrapers_front rect',1,200),shift(0,step6));
-setTimeout(slide('#skyscrapers_front rect',0,0,200),shift(0,step6));
-setTimeout(fadeFill('#skyscrapers_middle rect',1,200),shift(200,step6)); //themselves
-setTimeout(slide('#skyscrapers_middle rect',0,0,200),shift(200,step6));
-setTimeout(fadeFill('#skyscrapers_back rect',1,200),shift(400,step6));
-setTimeout(slide('#skyscrapers_back rect',0,0,200),shift(400,step6));
+setTimeout(function(){hydroPolesWiresIn(400)},shift(2600,step6));
+setTimeout(function(){hydroPolesWiresOut(400)},shift(4100,step6));
 
-setTimeout(function(){hydroPolesWiresIn(lengths,400)},shift(600,step6));
-setTimeout(function(){hydroPolesWiresOut(lengths,400)},shift(2100,step6));
+
+setTimeout(roadLit,shift(3500,step6));
+
+
+
 
 
 var off7 = 0;
@@ -347,7 +445,9 @@ setTimeout(function(){s.selectAll('radialGradient stop')[4].animate({stopOpacity
 setTimeout(function(){s.selectAll('radialGradient stop')[6].animate({stopOpacity:0.1},500)},shift(0,step10));
 
 setTimeout(function(){s.selectAll('radialGradient stop')[8].animate({stopOpacity:1},500)},shift(500,step10));
-setTimeout(fadeStroke('.constellation1,.constellation2',1,500),shift(800,step10));
+setTimeout(fadeStroke('.constellation1,.constellation2',1,0),shift(-700,step10));
+setTimeout(draw('#constellations path',0,false,true),shift(-700,step10));
+setTimeout(draw('#constellations path',700,false,false),shift(300,step10));
 
 
 
